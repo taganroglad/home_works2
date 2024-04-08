a39046b4e0afdb5d6328dc53468d03ba93877340
Alexey agafonov <taganroglad1@gmail.com>
2024-04-08 23:11:21 +0300

    efefef

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv('fifas2.csv')
+# Загрузка данных из файла CSV
+df = pd.read_csv('updated_fifa_s2.csv')
+
+print(df.isnull().sum())
+# Заполнение пропущенных значений в столбце 'Club' строкой 'Unknown'
+df['Club'] = df['Club'].fillna('Unknown')
+
+# Заполнение пропущенных значений в столбце 'Value' средним значением
+mean_value = df['Value'].mean()
+df['Value'] = df['Value'].fillna(mean_value)
